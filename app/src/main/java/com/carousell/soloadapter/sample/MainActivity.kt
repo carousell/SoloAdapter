44f464f792a0c40553ba75b75f855ae7dd4f26e0
Jintin <jintin.lin1018@gmail.com>
2020-08-12 10:33:46 +0800

    update example

--- app/src/main/java/com/carousell/soloadapter/sample/MainActivity.kt
+++ app/src/main/java/com/carousell/soloadapter/sample/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.carousell.soloadapter.sample
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.carousell.soloadapter.SoloAdapter
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,26 +15,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-
-        useCase1()
+        recyclerView.adapter = ConcatAdapter(adapter1(), adapter2(), adapter3())
     }
 
-    private fun useCase1() {
-        recyclerView.adapter = SoloAdapter(R.layout.layout_text)
+    private fun adapter1() = SoloAdapter(R.layout.layout_text)
+
+    private fun adapter2(): SoloAdapter {
+        val view = LayoutInflater.from(this).inflate(R.layout.layout_text, recyclerView, false)
+        view.findViewById<TextView>(R.id.textView).setText(R.string.simple_text)
+        return SoloAdapter(view)
     }
 
-    private fun useCase2() {
-        val text = TextView(this)
-        text.setText(R.string.simple_text)
-        recyclerView.adapter = SoloAdapter(text)
-    }
-
-    private fun useCase3() {
+    private fun adapter3(): SoloAdapter {
         val data = "New string"
         val adapter = SoloAdapter(R.layout.layout_text)
         adapter.bind { view ->
             view.findViewById<TextView>(R.id.textView).text = data
         }
-        recyclerView.adapter = adapter
+        return adapter
     }
 }
